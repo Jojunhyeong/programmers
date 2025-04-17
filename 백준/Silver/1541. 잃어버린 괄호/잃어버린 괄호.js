1450f73e60f6c63f62cdaf8b72e5a323d5b9c8b2
Josh <59139465+Jojunhyeong@users.noreply.github.com>
2025-04-17 21:29:10 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -4,15 +4,16 @@ const input = fs.readFileSync('/dev/stdin').toString().trim();
 function solution(expression) {
     const parts = expression.split('-');
 
+    // 첫 덩어리는 그냥 + 처리
     let result = parts[0]
         .split('+')
         .reduce((acc, cur) => acc + Number(cur), 0);
 
     for (let i = 1; i < parts.length; i++) {
-        const sum = parts[i]
+        const groupSum = parts[i]
             .split('+')
             .reduce((acc, cur) => acc + Number(cur), 0);
-        result -= sum;
+        result -= groupSum;
     }
 
     return result;
