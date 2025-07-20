d2f1085b6fcbd131fa995f470bcca43db780ba6d
Josh <59139465+Jojunhyeong@users.noreply.github.com>
2025-07-21 08:48:34 +0900

    [Bronze III] Title: 곱셈, Time: 88 ms, Memory: 9588 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.js
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const input = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
+const input = fs.readFileSync(0, 'utf-8').toString().split('\n');
 
 const A = parseInt(input[0]);
 const B = input[1];
