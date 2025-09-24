0a83a53c2d3923949d4bd4c6afabecc60fe469ab
Josh <59139465+Jojunhyeong@users.noreply.github.com>
2025-09-24 22:54:03 +0900

    [level 2] Title: 타겟 넘버, Time: 14.41 ms, Memory: 35.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,16 @@
+function solution(numbers, target) {
+  let count = 0;
+
+  function dfs(i, sum) {
+    if (i === numbers.length) {
+      if (sum === target) count += 1;
+      return;
+    }
+
+    dfs(i + 1, sum + numbers[i]);
+    dfs(i + 1, sum - numbers[i]);
+  }
+
+  dfs(0, 0);
+  return count;
+}
