7c9ea4584e554e7f3d4943f1aa2aeb87819dda0b
Josh <59139465+Jojunhyeong@users.noreply.github.com>
2025-09-25 16:35:44 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -1,18 +1,20 @@
+// 5키로랑 3키로 조합으로 담는 경우의 수를 찾은 다음에 5키로를 가장 많이 쓰는게 답 아닌가?
+
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim();
+const input = fs.readFileSync(0, "utf-8").toString().trim();
+
 const N = parseInt(input);
 
-console.log(solution(N));
-
-function solution(N) {
-    let x = Math.floor(N/5);
-    while(x >= 0){
-        let rest = N - 5 * x;
-        if(rest % 3 === 0){
-            let y = rest / 3;
-            return x + y;
+function solution(n) {
+    let count = 0;
+    while (n >=0) {
+        if(n % 5 === 0){
+            return count + n/5;
         }
-        x--;
+        n-=3;
+        count++;
     }
     return -1;
 }
+
+console.log(solution(N));
