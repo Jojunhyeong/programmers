e8783d36d57b6dde5717d16f32263f3e30e51a61
Josh <59139465+Jojunhyeong@users.noreply.github.com>
2025-10-23 09:33:00 +0900

    [Gold V] Title: 감소하는 수, Time: 96 ms, Memory: 9488 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const N = Number(fs.readFileSync(0, 'utf8').trim());
+
+if (N > 1022) {
+  console.log(-1);
+  process.exit(0);
+}
+
+const nums = [];
+
+// 현재 수 num의 마지막 자리보다 작은 숫자만 붙여서 다음 수를 만든다.
+function dfs(num, lastDigit) {
+  nums.push(num);
+  for (let d = 0; d < lastDigit; d++) {
+    dfs(num * 10 + d, d);
+  }
+}
+
+dfs(0, 0);
+for (let first = 1; first <= 9; first++) {
+  dfs(first, first);
+}
+
+nums.sort((a, b) => a - b);
+console.log(nums[N]);
